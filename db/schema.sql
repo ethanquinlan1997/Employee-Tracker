e7c118454a92a2f02cd308ade537aaf18cc6691f
Ethan Quinlan <ethan.quinlan2016@gmail.com>
2023-01-12 17:55:04 -0500

    added constraints

--- db/schema.sql
+++ db/schema.sql
@@ -16,6 +16,7 @@ CREATE TABLE roles (
     title VARCHAR(30)NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
+    CONSTRAINT fk_department
     FOREIGN KEY (department_id) 
     REFERENCES departments(id)
     ON DELETE SET NULL
@@ -28,8 +29,10 @@ CREATE TABLE employees (
     last_name VARCHAR (30)NOT NULL,
     role_id INT,
     manager_id INT,
+    CONSTRAINT fk_role
     FOREIGN KEY (role_id) 
     REFERENCES roles(id),
+    CONSTRAINT fk_employee
     FOREIGN KEY (manager_id) 
     REFERENCES employees(id)
      ON DELETE SET NULL
